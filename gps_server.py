891a5c7081e0b170af54866b80935accd6de0e07
kayla.hirano <kayla.hirano@spectrum-photonics.com>
2021-08-11 12:48:25 -1000

    second commit

--- gps_server.py
+++ gps_server.py
@@ -3,44 +3,69 @@ import threading
 import zmq
 import datetime
 import random
+import sys
+import serial
+import board
+from adafruit_lsm6ds.lsm6dsox import LSM6DSOX
+from picamera import PiCamera
 
-#IP = "169.254.210.83" #Ethernet 
-IP = "192.168.1.136" 
+IP = "169.254.16.177" #Ethernet 
+#IP = "192.168.1.164" 
 
-GPS = '$GPRMC,064951.000,A,2307.1256,N,12016.4438,E,0.03,165.48,260406,3.05,W,A*2C' #placeholder for actual info
+#GPS = '$GPRMC,064951.000,A,2307.1256,N,12016.4438,E,0.03,165.48,260406,3.05,W,A*2C' #placeholder for actual info
 
 ### FUNCTIONS ###
 
 def send_location(context):
     publisher = context.socket(zmq.PUB)
-    publisher.bind(f"tcp://{IP}:556")
+    try:
+        publisher.bind(f"tcp://{IP}:5556")
+    except:
+        print("Can't bind publisher")
+        sys.exit()
 
-    #gps = serial.Serial('/dev/serial0', 9600)
+    gps = serial.Serial('/dev/serial0', 9600)
+    i2c = board.I2C()  # uses board.SCL and board.SDA
+    sensor = LSM6DSOX(i2c)
+    
     # format and send data in a loop
     while True:
         current_date = datetime.datetime.now()
         formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
 
-        # Use placeholder for now
-        location_string = GPS
-        #gps_info = gps.readline().decode()
+        location_string = gps.readline().decode()
         if location_string[:6] == '$GPRMC':
             location_array = location_string.split(',')
-            latitude = location_array[3] + location_array[4]
-            longitude = location_array[5] + location_array[6]
+            latitude = location_array[3][:-7] + ' ' + location_array[3][-7:] + ' ' + location_array[4]
+            longitude = location_array[5][:-7] + ' ' + location_array[5][-7:] + ' ' + location_array[6]
 
-            location = f'[{formatted_date}] Latitude: {latitude}  Longitude: {longitude}'
+            #send location to client
+            location = f'{formatted_date}, Latitude: {latitude}, Longitude: {longitude}'
             publisher.send_string(f"LOCATION {location}")
             location_to_send = location
             print("Sent [%s] " % location_to_send)
+            
+            #send acceleration and gyro to client
+            #TODO: calculate pitch, yaw, and roll
+            accel = "Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (sensor.acceleration)
+            publisher.send_string(f"ACCEL {accel}")
+            print("Sent [%s] " % accel)
+            gyro = "Gyro X:%.2f, Y: %.2f, Z: %.2f radians/s" % (sensor.gyro)
+            publisher.send_string(f"GYRO {gyro}")
+            print("Sent [%s] " % gyro)
 
-        #  wait for 5 seconds
-        time.sleep(5)
+            #  wait for 5 seconds
+            time.sleep(5)
 
 def rand_reply(context):
     replier = context.socket(zmq.REP)
-    replier.bind(f"tcp://{IP}:555")
-
+    try:
+        replier.bind(f"tcp://{IP}:5555")
+    except:
+        print("Can't bind replier")
+        sys.exit()
+    
+    camera = PiCamera()
     while True:
         #  Wait for next request from client
         message = replier.recv_string()
@@ -48,10 +73,14 @@ def rand_reply(context):
         #  Do some 'work'
         time.sleep(1)
 
-        if message == 'RANDOM':
-            rand_num = random.randint(1, 100)
-            replier.send_string(str(rand_num))
-            to_client = rand_num
+        if message == 'SNAP':
+            camera.start_preview()
+            sleep(5)
+            #take picture
+            camera.capture('/share/test_image%s.jpg' %i)
+            camera.stop_preview()
+            i = i + 1
+            to_client = 'picture'
         elif message == 'STOP':
             replier.send_string('STOP')
             to_client = 'STOP'
@@ -67,6 +96,7 @@ def rand_reply(context):
 if __name__ == '__main__':
     # Prepare our context and sockets
     context = zmq.Context()
+    i = 1
 
     #threading
     t1 = threading.Thread(target=send_location, args=[context])
