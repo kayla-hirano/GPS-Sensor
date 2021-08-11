7a2df0298a5d955ed2d23356bea9ae2cc70ae60c
khirano7 <69174934+khirano7@users.noreply.github.com>
2021-08-11 12:45:22 -1000

    second commit

--- gps_client.py
+++ gps_client.py
@@ -3,8 +3,8 @@ import sys
 import threading
 import time
 
-#IP = '169.254.210.83' #Ethernet 
-IP = "192.168.1.136" 
+IP = '169.254.16.177' #Ethernet
+#IP = "192.168.1.10" 
 
 filter = 'LOCATION'
 
@@ -12,30 +12,37 @@ filter = 'LOCATION'
 
 def append_location(context):
     subscriber = context.socket(zmq.SUB)
-    subscriber.connect(f'tcp://{IP}:556')
+    subscriber.connect(f'tcp://{IP}:5556')
     subscriber.setsockopt_string(zmq.SUBSCRIBE, filter)
+    subscriber.setsockopt_string(zmq.SUBSCRIBE, 'ACCEL')
+    subscriber.setsockopt_string(zmq.SUBSCRIBE, 'GYRO')
 
     #append date in loop
     while True:
         string = subscriber.recv_string()
+        if 'LOCATION' in string:
+            gps_values = string.split(',')
+            AC_LAT = gps_values[1][11:]
+            AC_LONG = gps_values[2][12:]
+            gps_time = gps_values[0][9:]
+
         #write date in text file
-        file = open('gps.txt', 'a')
-        file.write(f'{string[9:]} \n')
+        file = open('data.txt', 'a')
+        file.write(f'{string} \n')
         file.close()
     
-def request_rand(context):
+def request_photo(context):
     requester = context.socket(zmq.REQ)
-    requester.connect(f'tcp://{IP}:555')
+    requester.connect(f'tcp://{IP}:5555')
     
     #print command list
     command = ''
-    time.sleep(1)
     print("""
         Please select from the following commands:\n
-        --------------------------------------------------\n
-        --- RANDOM for random number between 1 and 100 ---\n
-        --- STOP to disconnect ---------------------------\n
-        --------------------------------------------------""")
+        ------------------------------------------\n
+        --- SNAP to take a picture ---------------\n
+        --- STOP to disconnect -------------------\n
+        ------------------------------------------""")
     # Keep running until STOP command
     while command != 'STOP':
         #receive command from user
@@ -48,15 +55,28 @@ def request_rand(context):
         print("Data received from the server: [ %s ]" % message)
 
     print('STOPPED...')
+    #requester.close()
+    #context.term()
 
 ### CODE ###
 
 if __name__ == '__main__':
+    #define variables
+    AC_LAT = ''
+    AC_LONG = ''
+    AC_yaw = ''
+    ac_PITCH = 0
+    ac_roll = 0
+    sens_yaw = 0
+    sens_roll = ''
+    sens_pitch = ''
+    gps_time = ''
+
     context = zmq.Context()
 
     #threading
     t1 = threading.Thread(target=append_location, args=[context])
-    t2 = threading.Thread(target=request_rand, args=[context])
+    t2 = threading.Thread(target=request_photo, args=[context])
 
     t1.start()
     t2.start()
