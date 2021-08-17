69c142781563270c800b5322308ab8e614f146f1
khirano7 <69174934+khirano7@users.noreply.github.com>
2021-08-17 12:19:52 -1000

    fourth push

--- gps_client.py
+++ gps_client.py
@@ -2,6 +2,7 @@ import zmq
 import sys
 import threading
 import time
+import math
 
 IP = '169.254.16.177' #Ethernet
 #IP = "192.168.1.10" 
@@ -10,31 +11,31 @@ filter = 'LOCATION'
 
 ### FUNCTIONS ###
 
-def append_location(context):
-    subscriber = context.socket(zmq.SUB)
-    subscriber.connect(f'tcp://{IP}:5556')
-    subscriber.setsockopt_string(zmq.SUBSCRIBE, filter)
-    subscriber.setsockopt_string(zmq.SUBSCRIBE, 'ACCEL')
-    subscriber.setsockopt_string(zmq.SUBSCRIBE, 'GYRO')
-
+def append_location(context, subscriber):
     #append date in loop
     while True:
         string = subscriber.recv_string()
         if 'LOCATION' in string:
             gps_values = string.split(',')
-            AC_LAT = gps_values[1][11:]
-            AC_LONG = gps_values[2][12:]
+            AC_LAT = gps_values[1]
+            AC_LONG = gps_values[2]
             gps_time = gps_values[0][9:]
+            string = f'[{gps_time}] LAT: {AC_LAT} LONG: {AC_LONG}'
+        if 'ACCEL' in string:
+            sensor_data = string.split(',')
+            accel_x = float(sensor_data[0][6:])
+            accel_y = float(sensor_data[1])
+            accel_z = float(sensor_data[2])
+            sens_pitch = -(math.atan2(accel_x, math.sqrt(accel_y*accel_y + accel_z*accel_z))*180.0)/math.pi
+            sens_roll = (math.atan2(accel_y, accel_z)*180.0)/math.pi
+            string = f'[ACCEL] X:{accel_x} Y:{accel_y} Z:{accel_z} m/s^2'
 
         #write date in text file
         file = open('data.txt', 'a')
         file.write(f'{string} \n')
         file.close()
     
-def request_photo(context):
-    requester = context.socket(zmq.REQ)
-    requester.connect(f'tcp://{IP}:5555')
-    
+def request_photo(context, requester):
     #print command list
     command = ''
     print("""
@@ -50,38 +51,58 @@ def request_photo(context):
         print("Sending request for %s" %command)
         requester.send_string(command)
 
-        #  Get the reply.
+        #Get the reply.
         message = requester.recv_string()
-        print("Data received from the server: [ %s ]" % message)
+        print(message)
+    raise Exception('Requested program to stop')
 
-    print('STOPPED...')
-    #requester.close()
-    #context.term()
 
 ### CODE ###
 
 if __name__ == '__main__':
-    #define variables
-    AC_LAT = ''
-    AC_LONG = ''
-    AC_yaw = ''
-    ac_PITCH = 0
-    ac_roll = 0
-    sens_yaw = 0
-    sens_roll = ''
-    sens_pitch = ''
-    gps_time = ''
+    try:
+        context = zmq.Context()
+        #connect sockets
+        subscriber = context.socket(zmq.SUB)
+        requester = context.socket(zmq.REQ)
+        try:
+            subscriber.connect(f'tcp://{IP}:5556')
+            requester.connect(f'tcp://{IP}:5555')
+        except:
+            print("Can't connect sockets")
+            sys.exit()
+        subscriber.setsockopt_string(zmq.SUBSCRIBE, filter)
+        subscriber.setsockopt_string(zmq.SUBSCRIBE, 'ACCEL')
+        subscriber.setsockopt_string(zmq.SUBSCRIBE, 'GYRO')
+     
 
-    context = zmq.Context()
+        #define variables
+        AC_LAT = ''
+        AC_LONG = ''
+        AC_yaw = 0
+        ac_PITCH = 0
+        ac_roll = 0
+        sens_yaw = 0
+        sens_roll = ''
+        sens_pitch = ''
+        gps_time = ''
 
-    #threading
-    t1 = threading.Thread(target=append_location, args=[context])
-    t2 = threading.Thread(target=request_photo, args=[context])
+        #threading
+        t1 = threading.Thread(target=append_location, args=[context, subscriber])
+        t2 = threading.Thread(target=request_photo, args=[context, requester])
 
-    t1.start()
-    t2.start()
+        t1.start()
+        t2.start()
 
-    t1.join()
-    t2.join()
-
-    print('Done')
+    except (Exception):
+        print('Program Stopped...')
+    finally:
+        #close threads
+        t1.join()
+        t2.join()
+        #close sockets
+        requester.close()
+        subscriber.close()
+        #terminate context
+        context.term()
+        print('Done')
--- gps_server.py
+++ gps_server.py
@@ -10,20 +10,13 @@ from adafruit_lsm6ds.lsm6dsox import LSM6DSOX
 from picamera import PiCamera
 
 IP = "169.254.16.177" #Ethernet 
-#IP = "192.168.1.164" 
+#IP = "192.168.1.11" 
 
 #GPS = '$GPRMC,064951.000,A,2307.1256,N,12016.4438,E,0.03,165.48,260406,3.05,W,A*2C' #placeholder for actual info
 
 ### FUNCTIONS ###
 
-def send_location(context):
-    publisher = context.socket(zmq.PUB)
-    try:
-        publisher.bind(f"tcp://{IP}:5556")
-    except:
-        print("Can't bind publisher")
-        sys.exit()
-
+def send_location(context, publisher):
     gps = serial.Serial('/dev/serial0', 9600)
     i2c = board.I2C()  # uses board.SCL and board.SDA
     sensor = LSM6DSOX(i2c)
@@ -40,18 +33,16 @@ def send_location(context):
             longitude = location_array[5][:-7] + ' ' + location_array[5][-7:] + ' ' + location_array[6]
 
             #send location to client
-            location = f'{formatted_date}, Latitude: {latitude}, Longitude: {longitude}'
+            location = f'{formatted_date},{latitude},{longitude}'
             publisher.send_string(f"LOCATION {location}")
-            location_to_send = location
-            print("Sent [%s] " % location_to_send)
+            print("Sent [%s] " % location)
             
             #send acceleration and gyro to client
-            #TODO: calculate pitch, yaw, and roll
             accel_x = sensor.acceleration[0]
             accel_y = sensor.acceleration[1]
             accel_z = sensor.acceleration[2]
-            accel = f'ACCEL {accel_x},{accel_y},{accel_z}'
-            publisher.send_string(accel)
+            accel = f'{accel_x},{accel_y},{accel_z}'
+            publisher.send_string(f'ACCEL {accel}')
             print("Sent [%s] m/s^2" % accel)
             gyro = "Gyro X:%.2f, Y: %.2f, Z: %.2f radians/s" % (sensor.gyro)
             publisher.send_string(f"GYRO {sensor.gyro}")
@@ -60,22 +51,13 @@ def send_location(context):
             #  wait for 5 seconds
             time.sleep(5)
 
-def rand_reply(context):
-    replier = context.socket(zmq.REP)
-    try:
-        replier.bind(f"tcp://{IP}:5555")
-    except:
-        print("Can't bind replier")
-        sys.exit()
-    
+def rand_reply(context, replier): 
     camera = PiCamera()
     i = 1
     while True:
         #  Wait for next request from client
         message = replier.recv_string()
         print("Received request: %s" % message)
-        #  Do some 'work'
-        time.sleep(1)
 
         if message == 'SNAP':
             camera.start_preview()
@@ -85,8 +67,16 @@ def rand_reply(context):
             camera.capture(f'/share/{image_name}')
             camera.stop_preview()
             i = i + 1
+
+            #get current location
+            location_string = gps.readline().decode()
+            if location_string[:6] == '$GPRMC':
+                location_array = location_string.split(',')
+                latitude = location_array[3][:-7] + ' ' + location_array[3][-7:] + ' ' + location_array[4]
+                longitude = location_array[5][:-7] + ' ' + location_array[5][-7:] + ' ' + location_array[6]
+
             #format results
-            image_details = f'AMAP_LOAD_IMG_OL:[{image_name}]:[DISPLAYWINDOW]:[LAT1]:[LONG1]:[LAT2]:[LONG2]:'
+            image_details = f'AMAP_LOAD_IMG_OL:[{image_name}]:[DISPLAYWINDOW]:[{latitude}]:[{longitude}]:[LAT2]:[LONG2]:'
             replier.send_string(image_details)
             print(f'Sent {image_details}')
         elif message == 'STOP':
@@ -101,18 +91,34 @@ def rand_reply(context):
 ### CODE ###
 
 if __name__ == '__main__':
-    # Prepare our context and sockets
-    context = zmq.Context()
+    try:
+        # Prepare our context and sockets
+        context = zmq.Context()
+        publisher = context.socket(zmq.PUB)
+        replier = context.socket(zmq.REP)
+        try:
+            publisher.bind(f"tcp://{IP}:5556")
+            replier.bind(f"tcp://{IP}:5555")
+        except:
+            print("Can't bind sockets")
+            sys.exit()
     
+        #threading
+        t1 = threading.Thread(target=send_location, args=[context, publisher])
+        t2 = threading.Thread(target=rand_reply, args=[context, replier])
+        t1.start()
+        t2.start()
 
-    #threading
-    t1 = threading.Thread(target=send_location, args=[context])
-    t2 = threading.Thread(target=rand_reply, args=[context])
+    except KeyboardInterrupt:
+        print("Program interrupted...")
 
-    t1.start()
-    t2.start()
-
-    t1.join()
-    t2.join()
-
-    print('Done')
+    finally:
+        #end threading
+        t1.join()
+        t2.join()
+        #close sockets
+        publisher.close()
+        replier.close()
+        #terminate context
+        context.term()
+        print('Done.')
