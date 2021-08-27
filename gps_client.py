98b73d28d486acadf0348b64d38ac32f594dd915
khirano7 <69174934+khirano7@users.noreply.github.com>
2021-08-26 15:55:23 -1000

    fifth commit

--- gps_client.py
+++ gps_client.py
@@ -2,7 +2,7 @@ import zmq
 import sys
 import threading
 import time
-import math
+import sqlite3
 
 IP = '169.254.16.177' #Ethernet
 #IP = "192.168.1.10" 
@@ -12,28 +12,71 @@ filter = 'LOCATION'
 ### FUNCTIONS ###
 
 def append_location(context, subscriber):
+    #define variables
+    AC_LAT = ''
+    AC_LONG = ''
+    AC_yaw = 0
+    ac_PITCH = 0
+    ac_roll = 0
+    sens_yaw = 0
+    sens_roll = ''
+    sens_pitch = ''
+    gps_time = ''
+    id = 0
+
+    #connect to database
+    conn = sqlite3.connect('gpsinfo.db')
+    cur = conn.cursor() 
+    ## TEMPORARY FOR TESTING ##
+    cur.execute("DROP TABLE IF EXISTS info")
+    conn.commit()
+    ###########################
+    #create table of results
+    cur.execute("""CREATE TABLE IF NOT EXISTS info(
+        id INT PRIMARY KEY,
+        gps_time TEXT,
+        AC_LAT TEXT,
+        AC_LONG TEXT,
+        AC_yaw TEXT,
+        ac_PITCH TEXT,
+        ac_roll TEXT,
+        sens_yaw TEXT,
+        sens_roll TEXT,
+        sens_pitch TEXT)
+    """)
+    conn.commit()
+
     #append date in loop
     while True:
         string = subscriber.recv_string()
-        if 'LOCATION' in string:
-            gps_values = string.split(',')
-            AC_LAT = gps_values[1]
-            AC_LONG = gps_values[2]
-            gps_time = gps_values[0][9:]
-            string = f'[{gps_time}] LAT: {AC_LAT} LONG: {AC_LONG}'
-        if 'ACCEL' in string:
-            sensor_data = string.split(',')
-            accel_x = float(sensor_data[0][6:])
-            accel_y = float(sensor_data[1])
-            accel_z = float(sensor_data[2])
-            sens_pitch = -(math.atan2(accel_x, math.sqrt(accel_y*accel_y + accel_z*accel_z))*180.0)/math.pi
-            sens_roll = (math.atan2(accel_y, accel_z)*180.0)/math.pi
-            string = f'[ACCEL] X:{accel_x} Y:{accel_y} Z:{accel_z} m/s^2'
+        string = string.split(',')
+        gps_time = string[1]
+        AC_LAT = string[2]
+        AC_LONG = string[3]
+        #string = f'[{gps_time}] LAT: {AC_LAT} LONG: {AC_LONG}' 
+        accel_x = string[4]
+        accel_y = string[5]
+        accel_z = string[6]
+        sens_pitch = string[7]
+        sens_roll = string[8]
+        #string = f'[ACCEL] X:{accel_x} Y:{accel_y} Z:{accel_z} m/s^2'
 
         #write date in text file
-        file = open('data.txt', 'a')
-        file.write(f'{string} \n')
-        file.close()
+        #file = open('data.txt', 'a')
+        #file.write(f'{string} \n')
+        #file.close()
+
+        #write data to sql database
+        entry = (id, gps_time, AC_LAT, AC_LONG, AC_yaw, ac_PITCH, ac_roll, sens_yaw, sens_roll, sens_pitch)
+        cur.execute("INSERT INTO info VALUES(?,?,?,?,?,?,?,?,?,?);", entry)
+        conn.commit()
+        #print table
+        #cur.execute("SELECT * FROM info;")
+        #results = cur.fetchall()
+        #print(results)
+        
+        id += 1
+        
     
 def request_photo(context, requester):
     #print command list
@@ -61,8 +104,8 @@ def request_photo(context, requester):
 
 if __name__ == '__main__':
     try:
-        context = zmq.Context()
         #connect sockets
+        context = zmq.Context()
         subscriber = context.socket(zmq.SUB)
         requester = context.socket(zmq.REQ)
         try:
@@ -72,20 +115,7 @@ if __name__ == '__main__':
             print("Can't connect sockets")
             sys.exit()
         subscriber.setsockopt_string(zmq.SUBSCRIBE, filter)
-        subscriber.setsockopt_string(zmq.SUBSCRIBE, 'ACCEL')
-        subscriber.setsockopt_string(zmq.SUBSCRIBE, 'GYRO')
-     
-
-        #define variables
-        AC_LAT = ''
-        AC_LONG = ''
-        AC_yaw = 0
-        ac_PITCH = 0
-        ac_roll = 0
-        sens_yaw = 0
-        sens_roll = ''
-        sens_pitch = ''
-        gps_time = ''
+        subscriber.setsockopt_string(zmq.SUBSCRIBE, 'DATA')
 
         #threading
         t1 = threading.Thread(target=append_location, args=[context, subscriber])
